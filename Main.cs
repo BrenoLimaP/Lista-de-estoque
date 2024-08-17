ce1b47bc48d3c65b1dd75e8b0ee46b09f811bc37
Breno de Lima <98281131+BrenoLimaP@users.noreply.github.com>
2024-08-16 21:34:37 -0300

    Menu de Lista
    Aqui estarei colocando o menu, mas no momento so um introdução do que tenho que fazer

---
+++ Main.cs
@@ -0,0 +1,11 @@
+// Trabalho da faculdade - PTI
+// Fazer o estoque de uma empresa com produtos de minhas escolha
+// Declarar mais de 3 topicos para cada item, por ex: Livro= Autor, cor, e tema;
+
+
+// Primeiro passo criar um menu de seleção:
+// 1-Novo / 2-Listar Produtos / 3-Remover Produtos / 4-Entrada Estoque / 5-Saida Estoque / 0-Sair
+// O menu precisa ser recursivo, ou seja, precisa aparecer até que seja teclado 0 ou sair
+
+
+// Menu:
