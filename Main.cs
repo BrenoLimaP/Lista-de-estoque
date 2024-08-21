41078ac512a936dce10d1404c501726af1e86ed9
Breno de Lima <98281131+BrenoLimaP@users.noreply.github.com>
2024-08-21 15:03:07 -0300

    Menu
    Um pequeno avanço para finalizar o menu

--- Main.cs
+++ Main.cs
@@ -8,30 +8,60 @@ using System;
 // 1-Novo / 2-Listar Produtos / 3-Remover Produtos / 4-Entrada Estoque / 5-Saida Estoque / 0-Sair
 // O menu precisa ser recursivo, ou seja, precisa aparecer até que seja teclado 0 ou sair
 
-
 // Menu
-class Program{
+class Program{  
+    // Coleta de dados
+    // Processamento de Dados
+    // Saida de dados
     static void Main(){
-        Console.WriteLine("Escolha uma acao:");
-        Console.WriteLine("1 - Novo Pedido");
-        Menu();
+        Console.WriteLine(@" Menu de Opções: 
+        1.Novo
+        2.Listar Produtos
+        3.Remover Produtos
+        4.Entrada Estoque
+        5.Saida Estoque
+        0.Sair");
+        int escolha = Convert.ToInt32(Console.ReadLine()); 
+        //int estoque = 0;
 
-        if (res == 1){
-            Console.WriteLine("Descreva o pedido:");
-            res2 = Convert.ToString(Console.ReadLine());
+       switch (escolha){
+            case 1:
+                Console.WriteLine("Descreva Nome do produto: ");
+                string produto = Convert.ToString(Console.ReadLine());
+                Console.WriteLine("Seu produto se chama: " + produto);
+                break;
             
-        }
-        else {
-            Console.WriteLine("caiu no else");
-        }
+            case 2:
+                Console.WriteLine("Lista de produtos");
+                Console.WriteLine(produto);
+                break;
+
+            case 3:
+                Console.WriteLine("Remova produtos");
+                int num3 = Convert.ToInt32(Console.ReadLine());
+                break;
+
+            case 4:
+                Console.WriteLine("entrada de estoque");
+                int num4 = Convert.ToInt32(Console.ReadLine());
+                break;
+
+            case 5:
+                Console.WriteLine("Saida do estoque");
+                int num5 = Convert.ToInt32(Console.ReadLine());
+                break;
+
+            case 0:
+                Console.WriteLine("Sair do menu");
+                int num6 = Convert.ToInt32(Console.ReadLine());
+                break;
+       }
+
+       //Console.WriteLine("Numero escolhido " + escolha);
     }
 }
 // Irei fazer o menu em uma função separada para poder reutili-zar
-    static int Menu(res){
-        Console.WriteLine("1 - Novo Pedido");
-        int res = Convert.ToInt32(Console.ReadLine());
 
-    }
 
 
 
